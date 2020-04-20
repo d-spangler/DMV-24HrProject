3d4fc36d0887c76e0ca496735fbd72631de55ca7
M1K00 <61482244+M1K00@users.noreply.github.com>
2020-04-20 17:05:47 -0400

    fixing bugs

--- 24HourProjectAPI/Controllers/PostController.cs
+++ 24HourProjectAPI/Controllers/PostController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _24HourProjectModels;
+using _24HourProjectServices;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,32 @@ using System.Web.Http;
 
 namespace _24HourProjectAPI.Controllers
 {
+    [Authorize]
     public class PostController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new PostService(userId);
+            return noteService;
+        }
     }
 }

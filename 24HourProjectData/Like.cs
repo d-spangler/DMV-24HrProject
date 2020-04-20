49e40fdaa7c8ccda07cbbea90630b15bd65151d8
d-spangler <61887496+d-spangler@users.noreply.github.com>
2020-04-20 15:24:41 -0400

    user and like class

---
+++ 24HourProjectData/Like.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProjectData
+{
+    public class Like
+    {
+        [ForeignKey(nameof(LikedPost))]
+        public virtual bool LikedPost { get; set; }
+
+        [ForeignKey(nameof(Liker))]
+        public virtual string Liker { get; set; }
+    }
+}
